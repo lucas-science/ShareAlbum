7605f52bd550e831ce42e6ee4e2f214b151a62af
lucas-science <lucaslhomme01@gmail.com>
2024-09-13 21:46:18 +0200

    modif

--- client/src/pages/Confidentialite.jsx
+++ client/src/pages/Confidentialite.jsx
@@ -56,9 +56,16 @@ const PrivacyPolicy = () => {
         Pour toute question ou préoccupation concernant notre politique de confidentialité, veuillez nous contacter à <strong>lucaslhomme01@gmail.com</strong>.
       </p>
     </div>
-    <div className="bg-gray-100 flex items-center justify-center min-h-screen">
-    <div className="w-12 h-12 border-4 border-t-4 border-t-blue-500 border-transparent rounded-full animate-spin"></div>
-    </div>
+    <div className="bg-gray-100 flex items-center justify-center min-h-screen space-x-4">
+    {/* Cercle 1 avec animation rapide */}
+    <div className="w-10 h-10 bg-blue-500 rounded-full animate-ping-fast"></div>
+
+    {/* Cercle 2 avec animation moyenne */}
+    <div className="w-10 h-10 bg-blue-500 rounded-full animate-ping-medium"></div>
+
+    {/* Cercle 3 avec animation lente */}
+    <div className="w-10 h-10 bg-blue-500 rounded-full animate-ping-slow"></div>
+  </div>
     </>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'ping-slow': 'ping 1.5s cubic-bezier(0, 0, 0.2, 1) infinite',
+        'ping-medium': 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite',
+        'ping-fast': 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+      }
+    },
   },
   plugins: [],
 }
