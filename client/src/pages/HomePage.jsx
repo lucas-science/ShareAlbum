8bc1cfbfbce7b6789665148f5956a281f4099e99
lucas-science <lucaslhomme01@gmail.com>
2024-09-20 20:38:43 +0200

    modif

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -14,7 +14,7 @@ class HomePage extends Component {
         <div className="min-h-screen bg-gray-100 flex flex-col justify-between">
           <div className="flex-grow flex items-center justify-center">
             <div className="bg-white p-8 rounded shadow-md text-center flex flex-col">
-              <h1 className="text-2xl font-bold text-gray-800">Welcome to Album photo share!</h1>
+              <h1 className="text-2xl font-bold text-gray-800">Welcome to HomeSnapshots!</h1>
               <a href={this.create_url} rel="noreferrer">
                 <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
                   Create your Home Album
